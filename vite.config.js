a1ee5b148d2ae05bcedec0a1ab0814448fd13152
Mohamed Sameer <md.sameer.dev@gmail.com>
2025-02-04 22:59:42 +0530

    Add build output

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Customize the output directory for the build
+  }
 })
